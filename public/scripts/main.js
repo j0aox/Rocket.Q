435c96a789eb8f8d0b4d507b87ba17f4b69de61b
João Amador <nnettoolliveira@gmail.com>
2022-06-10 07:52:42 -0300

    funcionalidades de abrir e fechar modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,13 +2,32 @@ import Modal from './modal.js';
 
 const modal = Modal();
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
+
 //Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check");
 
 checkButtons.forEach(button => {
     //adicionar a escuta
-    button.addEventListener("click", event => {
-        //Abrir modal
-        modal.open();
-    });
+    button.addEventListener("click", handleClick);
 });
+
+//Quando o botão delete for clicado ele abre a modal
+const deleteButton = document.querySelectorAll(".actions a.delete");
+
+deleteButton.forEach(button => {
+    //adicionar a escuta
+    button.addEventListener("click", (event) => handleClick(event, false));
+});
+
+function handleClick(event, check = true) {
+    const text = check ? "Marcar como lida" : "Excluir";
+
+    modalTitle.innerHTML = `${text} esta pergunta`;
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`;
+
+    //abrir modal
+    modal.open();
+}
