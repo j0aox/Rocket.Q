0f73c77405895150920c800e5a57e19d7a9a1dfe
João Amador <nnettoolliveira@gmail.com>
2022-06-22 16:49:26 -0300

    número da sala não duplicado

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,19 +5,28 @@ module.exports = {
         const db = await Database();
         const pass = req.body.password;
         let roomId;
+        let isRoom = true;
 
-        for (var i = 0; i < 6; i++) {
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-                roomId += Math.floor(Math.random() * 10).toString();
+        while (isRoom) {
+            // Gera o numero da sala
+            for (var i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                    roomId += Math.floor(Math.random() * 10).toString();
+            }
+
+            const roomsExistIds = await db.all(`SELECT * FROM rooms`);
+            isRoom = roomsExistIds.some(roomsExistIds => roomsExistIds === roomId);
+
+            if (!isRoom) {
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`);
+            }
         }
-
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`);
 
         await db.close();
 
